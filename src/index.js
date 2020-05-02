38dca4efe1781e56fa4afee2d8e0194366b20fb2
zhangbo <15947658171@163.com>
2020-05-02 22:11:21 +0800

    preview_12

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2020-05-02 21:25:19
- * @LastEditTime: 2020-05-02 21:39:25
+ * @LastEditTime: 2020-05-02 22:06:43
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: /web/level/level-react/src/index.js
@@ -10,16 +10,54 @@
 import React from 'react';
 // ReactDom渲染实际DOM，VDOM->DOM
 import ReactDOM from 'react-dom';
-import './index.css';
+// import './index.css';
+import sytles from './index.module.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// 1.基本使用 表达式用{}包裹
+const name = "React"
+
+// 2.函数的使用方式
+const obj = {
+  firstName: 'ZhangBo',
+  lastName: 'NiMo'
+}
+function handleName(obj) {
+  return obj.firstName + ' ' + obj.lastName;
+}
+
+// 3.JSX对象
+const greet = <div>good</div>
+
+// 4.条件语句
+const show = true
+
+// 5.数组
+const a = [0, 1, 2]
+
+const jsx = 
+<div className={sytles.app}>
+  <div>hello, {name}</div>
+  <div>{handleName(obj)}</div>
+  <div className={sytles.test}>{greet}</div>
+  <div>{show?greet:'登录'}</div>
+  <ul>
+    {/* diff时候，首先比较type，然后是key，所以同级同类型元素，key值必须唯一 */}
+    {a.map(item => (
+      <li key={item}>{item}</li>
+    ))}
+  </ul>
+</div>
+
+ReactDOM.render(jsx, document.getElementById("root"));
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
