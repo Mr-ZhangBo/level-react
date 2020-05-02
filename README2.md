fcf83e6ab1c3fa2567a9ca5674597118261ee6aa
zhangbo <15947658171@163.com>
2020-05-02 21:40:36 +0800

    preview_1

---
+++ README2.md
@@ -0,0 +1,21 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-05-02 21:31:32
+ * @LastEditTime: 2020-05-02 21:33:33
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /web/level/level-react/README2.md
+ -->
+```
+    // 1.创建项⽬目
+    npx create-react-app my-app
+
+    // 2.打开项⽬目
+    cd my-app
+
+    // 3.启动项⽬目
+    npm start
+
+    // 4.暴露配置项
+    npm run eject
+```
--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2020-05-02 21:25:19
+ * @LastEditTime: 2020-05-02 21:39:25
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /web/level/level-react/src/index.js
+ */
+// React负责逻辑通知，数据->VDOM,这里不能注释，JSX依赖于React
 import React from 'react';
+// ReactDom渲染实际DOM，VDOM->DOM
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
